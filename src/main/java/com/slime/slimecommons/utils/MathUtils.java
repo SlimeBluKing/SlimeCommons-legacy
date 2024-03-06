97914e3a91e596890bbc8f362e734f7299fea356
Stefano Tudose <stfntudose@gmail.com>
2024-03-06 21:25:27 +0100

    Version 0.1.4.5
    Aggiunto randomLong e randomInt su MathUtils (Aggiungerò altri tipi di dati in seguito)

--- src/main/java/com/slime/slimecommons/utils/MathUtils.java
+++ src/main/java/com/slime/slimecommons/utils/MathUtils.java
@@ -6,4 +6,11 @@ public class MathUtils {
     public static boolean tryPercentage(double percentage){
         return new Random().nextDouble() <= percentage;
     }
+    public static int randomInt(int min, int max){
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+
+    public static long randomLong(long min, long max){
+        return new Random().nextLong((max - min) + 1) + min;
+    }
 }
